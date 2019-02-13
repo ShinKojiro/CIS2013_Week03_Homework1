e68bfffefa357656f78914c0952cf91789418912
James Hall <42621442+ShinKojiro@users.noreply.github.com>
2019-02-12 22:59:30 -0600

    its a switch in a switch!

--- main.cpp
+++ main.cpp
@@ -13,46 +13,98 @@ int main(){
         cin >> playerOneChoice;
         cout << endl << "Player Two, choose your throw! - ";
         cin >> playerTwoChoice;
+        cout << endl;
 
         switch(playerOneChoice){
             case 'P':
             case 'p':
-                if(playerTwoChoice == 'p' || playerTwoChoice == 'P'){
-                    cout << "Tied, Paper vs Paper!";
+                switch(playerTwoChoice){
+                    case 'p':
+                    case 'P':
+                        cout << "Tied, Paper vs Paper!" << endl;
+                    break;
+                    case 's':
+                    case 'S':
+                        cout << "Player Two's Scissors Sliced through player One's paper!" << endl;
+                    break;
+                    case 'r':
+                    case 'R':
+                        cout << "Player One's Paper wraps up Player Two's rock nice and good!" << endl;
+                    break;
+                    default:
+                        cout << "Player Two didn't chose a valid option!" << endl
+                        << "R for Rock." << endl << "P for Paper" << endl << "S for Scissors" << endl;
+                }
+                /*if(playerTwoChoice == 'p' || playerTwoChoice == 'P'){
+                    cout << "Tied, Paper vs Paper!" << endl;
                 }
                 else if(playerTwoChoice == 's' || playerTwoChoice == 'S'){
-                    cout << "Player Two's Scissors Sliced through player One's paper!";
+                    cout << "Player Two's Scissors Sliced through player One's paper!" << endl;
                 }
                 else if(playerTwoChoice == 'r' || playerTwoChoice == 'R'){
-                    cout << "Player One's Paper wraps up Player Two's rock nice and good!";
-                }
+                    cout << "Player One's Paper wraps up Player Two's rock nice and good!" << endl;
+                }*/
             break;
             case 's':
             case 'S':
-                if(playerTwoChoice == 's' || playerTwoChoice == 'S'){
-                    cout << "Tied, Scissors vs Scissors!";
+                switch(playerTwoChoice){
+                    case 's':
+                    case 'S':
+                        cout << "Tied, Scissors vs Scissors!" << endl;
+                    break;
+                    case 'p':
+                    case 'P':
+                        cout << "Player One turned Player Two's Paper into tiny pieces!" << endl;
+                    break;
+                    case 'r':
+                    case 'R':
+                        cout << "Player Two Rock bends poor Player One scissors out of shape!" << endl;
+                    break;
+                    default:
+                        cout << "Player Two didn't chose a valid option!" << endl
+                        << "R for Rock." << endl << "P for Paper" << endl << "S for Scissors" << endl;
+                }
+                /*if(playerTwoChoice == 's' || playerTwoChoice == 'S'){
+                    cout << "Tied, Scissors vs Scissors!" << endl;
                 }
                 else if(playerTwoChoice == 'p' || playerTwoChoice == 'P'){
-                    cout << "Player One turned Player Two's Paper into tiny pieces!";
+                    cout << "Player One turned Player Two's Paper into tiny pieces!" << endl;
                 }
                 else if(playerTwoChoice == 'r' || playerTwoChoice == 'R'){
-                    cout << "Player Two Rock bends poor Player One scissors out of shape!";
-                }
+                    cout << "Player Two Rock bends poor Player One scissors out of shape!" << endl;
+                }*/
             break;
             case 'r':
             case 'R':
-                if(playerTwoChoice == 'r' || playerTwoChoice == 'R'){
-                    cout << "Tied, Rock vs Rock!";
+                switch(playerTwoChoice){
+                    case 'r':
+                    case 'R':
+                        cout << "Tied, Rock vs Rock!" <<endl;
+                    break;
+                    case 's':
+                    case 'S':
+                        cout << "Player One's Rock made Player Two's Scissors fubar!" << endl;
+                    break;
+                    case 'p':
+                    case 'P':
+                        cout << "Player Two's Paper erased Player One's Rock from existance!" << endl;
+                    break;
+                    default:
+                        cout << "Player Two didn't chose a valid option!" << endl
+                        << "R for Rock." << endl << "P for Paper" << endl << "S for Scissors" << endl;
+                }
+                /*if(playerTwoChoice == 'r' || playerTwoChoice == 'R'){
+                    cout << "Tied, Rock vs Rock!" <<endl;
                 }
                 else if(playerTwoChoice == 's' || playerTwoChoice == 'S'){
-                    cout << "Player One's Rock made Player Two's Scissors fubar!";
+                    cout << "Player One's Rock made Player Two's Scissors fubar!" << endl;
                 }
-                else if(playerTwoChoice == 'r' || playerTwoChoice == 'R'){
-                    cout << "Player Two's Paper erased Player One's Rock from existance!";
-                }
+                else if(playerTwoChoice == 'p' || playerTwoChoice == 'P'){
+                    cout << "Player Two's Paper erased Player One's Rock from existance!" << endl;
+                }*/
             break;
             default:
-                cout << "No valid letter was entered." << endl 
+                cout << "Player One didn't choose a valid option!" << endl 
                 << "R for Rock." << endl << "P for Paper" << endl << "S for Scissors" << endl;
         }
 
@@ -60,4 +112,5 @@ int main(){
         cin >> playAgain;
     }
 
+    cout << "\nThank you for playing!";
 }
